d1dbfc3e585e24256533a0394197ac4dec7e0829
IceWizard <mrrioes.dc@gmail.com>
2025-11-23 19:05:04 +0100

    Improved rename.py logic

--- commands/rename.py
+++ commands/rename.py
@@ -9,21 +9,8 @@ def run(args: typing.Any) -> None:
         print('Usage: rename <old_file> <new_file>')
         return
 
-    target = args[0]
-    if os.path.isdir(target):
+    if len(args) != 2:
         print('Usage: rename <old_file> <new_file>')
         return
-    else:
-        try:
-            with open(target, 'r') as file:
-                content = file.read()
-            os.remove(target)
-        except Exception as e:
-            print(f'Error: {e}')
-            return
 
-    try:
-        with open(args[1], 'w') as file:
-            file.write(content)
-    except Exception as e:
-        print(f'Error: {e}')
+    os.rename(args[0], args[1])
